d2a30f5a927544bf8a0fd070825d45b98c16566e
Misael Taperia <askedmisael2@gmail.com>
2025-03-08 21:34:56 -0600

    set window size and visibility

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 public class Main extends JFrame {
     public static void main(String[] args) {
-        
+        Main fr = new Main();
+        fr.setSize(100,100);
+        fr.setVisible(true);
     }
 }
